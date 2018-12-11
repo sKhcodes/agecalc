c3b1ce01494aace1ef25b3841da1842a18bc2f9b
Gary Siu <Gary.Siu@matchesfashion.com>
2018-12-11 20:21:38 +0000

    working version with moment.js

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
+var $button = document.querySelector(".btn");
+$button.addEventListener('click', handleClick);
 
-
-document.querySelector(".btn").onclick=showYourself;
-document.querySelector(".btn").onclick=showBirthday;
-
-
-function showYourself() {
-    let result= document.querySelector(".result");
-result.style.display="block";
-console.log("shown");
+function handleClick() {
+    showBirthday();
+    // showResult();
 }
 
-function hideAndSeek() {
-    document.querySelector(".result").style.display="none";
+// document.querySelector(".btn").onclick = showBirthday;
+
+
+function showResult(days) {
+    let parent = document.querySelector(".result");
+    let result = document.querySelector(".result span");
+    result.innerHTML = days;
+    parent.style.display = "block";
+    console.log("shown");
 }
-//window.onload =hideAndSeek();
+
+// function hideAndSeek() {
+//     document.querySelector(".result").style.display = "none";
+// }
+// window.onload =hideAndSeek();
 function showBirthday() {
     var birthday = document.forms["age__form"]["birthday"].value;
-    console.log(birthday);
-
-   
+    var days = moment().diff(moment(birthday), 'days');
+    showResult(days);
 }
-let date = new Date();
- let dd = date.getDate();
-  let mm = date.getMonth() + 1;
-  let yyyy = date.getFullYear();
-   if (dd < 10) {
-     dd = '0' + dd;
-   } 
-   if (mm < 10) {
-     mm = '0' + mm;
-   } 
-   let todaysDate = dd + '/' + mm + '/' + yyyy;
-   console.log(todaysDate);
+// let date = new Date();
+//  let dd = date.getDate();
+//   let mm = date.getMonth() + 1;
+//   let yyyy = date.getFullYear();
+//    if (dd < 10) {
+//      dd = '0' + dd;
+//    } 
+//    if (mm < 10) {
+//      mm = '0' + mm;
+//    } 
+//    let todaysDate = dd + '/' + mm + '/' + yyyy;
+//    console.log(todaysDate);
 
-/*  I need to 1. format both dates the same. 
-2. Figure out how to bridge months, years etc.
+// /*  I need to 1. format both dates the same. 
+// 2. Figure out how to bridge months, years etc.
